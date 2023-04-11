33c49fa89e3f866187cb52a37946ea1bd80a9a21
bdconnors <bdc5435@rit.edu>
2023-04-11 14:58:34 -0400

    add logging service to sites service and add http requests

--- apps/site/src/app/app.controller.ts
+++ apps/site/src/app/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post } from '@nestjs/common';
-import { Body, Param } from '@nestjs/common/decorators';
-import { Ctx, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
+import { Body, Inject, Param } from '@nestjs/common/decorators';
+import { ClientRMQ, Ctx, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
 import { AppService } from './app.service';
 import { AddSiteDto } from './dto/add-site.dto';
 
@@ -8,35 +8,50 @@ import { AddSiteDto } from './dto/add-site.dto';
 @Controller('sites')
 export class AppController {
 
-  constructor(private readonly service: AppService){}
-  
-  @MessagePattern('list')
-  list(@Payload() data: any, @Ctx() context: RmqContext) {
-    console.log(data);
-    return this.service.getAll();
-  }
-  @MessagePattern('find')
-  find(@Payload() data: any, @Ctx() context: RmqContext) {
-    console.log(data);
-    return this.service.get(data.id);
-  }
-  @MessagePattern('findRole')
-  findRole(@Payload() data: any, @Ctx() context: RmqContext) {
-    console.log(data);
-    return this.service.get(data.id);
-  }
-  /**@Get('/:id')
+  constructor(
+    private readonly service: AppService,
+    @Inject('LOGGER_SERVICE') private logger: ClientRMQ
+  ){}
+
+  @Get('/:id')
   async getSite(@Param('id') id: string) {
-    return await this.service.get(id);
+    const site = await this.service.get(id);
+    this.logger.emit('info', { method:'GET', action:'READ', description: 'get site request', data: site });
+    return site;
   }
 
   @Get()
   async listSites() {
-    return await this.service.getAll();
+    const sites = await this.service.getAll();
+    this.logger.emit('info', { method:'GET', action:'READ', description: 'get all sites request', data: sites });
+    return sites;
   }
 
   @Post()
   async addSite(@Body() dto: AddSiteDto){
-    return await this.service.add(dto.orgId, dto.name);
-  }**/
+    const site = await this.service.add(dto.orgId, dto.name);
+    this.logger.emit('info', { method:'POST', action:'CREATE', description: 'create site request', data: site });
+    return site;
+  }
+
+  @MessagePattern('list')
+  async list(@Payload() data: any) {
+    const sites = await this.service.getAll();
+    this.logger.emit('info', { method:'MSG', action:'READ', description: 'get all sites message', data: sites });
+    return sites;
+  }
+  
+  @MessagePattern('find')
+  async find(@Payload() data: any) {
+    const site = await this.service.get(data.id);
+    this.logger.emit('info', { method:'MSG', action:'READ', description: 'get site message', data: site });
+    return site;
+  }
+
+  @MessagePattern('findRole')
+  async findRole(@Payload() data: any) {
+    const role = await this.service.get(data.id);
+    this.logger.emit('info', { method:'MSG', action:'READ', description: 'get role message', data: role });
+    return role;
+  }
 }
--- apps/site/src/app/app.module.ts
+++ apps/site/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { AccountsModule, OrgModule, PermissionCollection, PermissionModule, RoleCollection, RoleModule, SiteCollection, SiteModule } from '@diamond/mongo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
@@ -8,6 +9,21 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ClientsModule.register(
+      [
+        { 
+          name: `LOGGER_SERVICE`, 
+          transport: Transport.RMQ,
+          options: {
+            urls: [process.env.RABBITMQ_URL],
+            queue: `LOGGER_QUEUE`,
+            queueOptions: {
+              durable: false
+            }
+          }
+        }
+      ]
+    ),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_DB_CONN),
     SiteModule,
